e9588df003b74f78bf098d9527d4185df8273940
mohammed <m2008m1033m@gmail.com>
2019-01-02 11:20:59 +1100

    init

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
+import 'dart:math';
+
+class Home extends StatefulWidget {
+  @override
+  State createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  Animation<double> boxAnimation;
+  AnimationController boxController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    boxController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+
+    boxAnimation = Tween(
+      begin: pi * 0.6,
+      end: pi * 0.65,
+    ).animate(
+      CurvedAnimation(
+        parent: boxController,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    boxAnimation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        boxController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        boxController.forward();
+      }
+    });
+    boxController.forward();
+
+    catController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 200),
+    );
+
+    catAnimation = Tween(
+      begin: -35.0,
+      end: -80.0,
+    ).animate(CurvedAnimation(
+      parent: catController,
+      curve: Curves.easeIn,
+    ));
+  }
+
+  void onTap() {
+    if (catAnimation.status == AnimationStatus.completed) {
+      boxController.forward();
+      catController.reverse();
+    } else if (catAnimation.status == AnimationStatus.dismissed) {
+      boxController.stop();
+      catController.forward();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Animation!',
+        ),
+      ),
+      body: GestureDetector(
+        onTap: onTap,
+        child: Center(
+          child: Stack(
+            overflow: Overflow.visible,
+            children: <Widget>[
+              buildCatAnimation(),
+              buildBox(),
+              buildLeftFlap(),
+              buildRightFlap(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildCatAnimation() {
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Positioned(
+          child: child,
+          right: 0.0,
+          left: 0.0,
+          top: catAnimation.value,
+        );
+      },
+      child: Cat(),
+    );
+  }
+
+  Widget buildBox() {
+    return Container(
+      color: Colors.brown,
+      height: 200.0,
+      width: 200.0,
+    );
+  }
+
+  Widget buildLeftFlap() {
+    return Positioned(
+      left: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        builder: (context, child) {
+          return Transform.rotate(
+            angle: boxAnimation.value,
+            alignment: Alignment.topLeft,
+            child: child,
+          );
+        },
+        child: Container(
+          color: Colors.brown,
+          height: 10.0,
+          width: 125.0,
+        ),
+      ),
+    );
+  }
+
+  Widget buildRightFlap() {
+    return Positioned(
+      right: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        builder: (context, child) {
+          return Transform.rotate(
+            angle: -boxAnimation.value,
+            alignment: Alignment.topRight,
+            child: child,
+          );
+        },
+        child: Container(
+          color: Colors.brown,
+          height: 10.0,
+          width: 125.0,
+        ),
+      ),
+    );
+  }
+}
